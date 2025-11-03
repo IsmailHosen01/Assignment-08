9c31a52ae102518f3a2c6598fe323ba497ccf3c6
IsmailHosen01 <ismailhossen12340@gmail.com>
2025-11-03 19:29:43 +0600

    Search feature implemented

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import AppCard from '../../components/AppCard/AppCard';
 
 const Apps = () => {
     const allAppsData = useLoaderData();
+    const [searchValue, setSearchValue] = useState('');
+
+    const filteredApps = allAppsData.filter(app => app.title.toLowerCase().includes(searchValue.toLowerCase()));
+    console.log(filteredApps);
+
     return (
         <div className='bg-base-200 max-w-[1440px] mx-auto'>
             <h3 className='text-center text-[#001931] text-5xl font-bold pt-10'>Our All Applications</h3>
             <p className='text-center text-xl text-[#627382] my-4'>Explore All Apps on the Market developed by us. We code for Millions</p>
 
             <div className='flex justify-between items-center'>
-                <p className='text-2xl font-semibold text-[#001931]'>({allAppsData.length}) Apps Found</p>
+                <p className='text-2xl font-semibold text-[#001931]'>({filteredApps.length}) Apps Found</p>
                 <label className="input">
                     <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <g
@@ -24,15 +29,25 @@ const Apps = () => {
                             <path d="m21 21-4.3-4.3"></path>
                         </g>
                     </svg>
-                    <input type="search" required placeholder="Search" />
+                    <input
+                        type="search"
+                        required
+                        placeholder="Search"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
                 </label>
             </div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-5'>
-                {
+                {/* {
                     allAppsData.map(data => <AppCard key={data.id} data={data}></AppCard>)
-                }
+                } */}
+
+                {filteredApps.length > 0 ? (filteredApps.map(data => <AppCard key={data.id} data={data}></AppCard>)) : (<p className='text-center text-gray-500 col-span-full text-2xl p-10'>No Apps Found</p>)}
             </div>
+
+
         </div>
     );
 };
